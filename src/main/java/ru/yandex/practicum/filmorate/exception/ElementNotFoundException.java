1873db4c96a70a1ba12c8dde4f172c8907d51170
ineptErick <inepterick@gmail.com>
2023-01-05 19:25:02 +0300

    New commit

--- src/main/java/ru/yandex/practicum/filmorate/exception/ElementNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package ru.yandex.practicum.filmorate.exception;
-
-public class ElementNotFoundException extends RuntimeException {
-    public ElementNotFoundException(String param) {
-        super(param);
-    }
-}
